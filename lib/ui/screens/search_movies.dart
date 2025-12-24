88b45c771dce3d618b13a6141a3b832e8ab5068c
Jose A F Santos <toni.farias@outlook.com>
2025-12-24 10:03:41 -0300

    Dashboard finalizada

--- lib/ui/screens/search_movies.dart
+++ lib/ui/screens/search_movies.dart
@@ -6,6 +6,46 @@ class SearchMovies extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: MovieCard());
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Image.asset(
+            "assets/images/popular.png",
+            height: 80,
+            width: 80,
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 40),
+            child: Text(
+              "Filmes Populares",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 32),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Pesquisar",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(18),
+                ),
+              ),
+            ),
+          ),
+        ),
+        SliverList.builder(
+          itemBuilder: (context, index) => Padding(
+            padding: const EdgeInsets.only(bottom: 32),
+            child: MovieCard(),
+          ),
+          itemCount: 10,
+        ),
+      ],
+    );
   }
 }
