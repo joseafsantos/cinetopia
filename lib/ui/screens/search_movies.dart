b4f24308ce15594644a3b8cdd90449533d581867
Jose A F Santos <toni.farias@outlook.com>
2025-12-24 09:17:39 -0300

    Dashboard criada

---
+++ lib/ui/screens/search_movies.dart
@@ -0,0 +1,11 @@
+import '/ui/components/movie_card.dart';
+import 'package:flutter/material.dart';
+
+class SearchMovies extends StatelessWidget {
+  const SearchMovies({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: MovieCard());
+  }
+}
